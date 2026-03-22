6ca1008db7f2f4989c29da69822fd02b33d6a7b4
arciay <32655163@qq.com>
2026-03-23 05:11:09 +0800

    推送ad

---
+++ Test.lua
@@ -0,0 +1,7 @@
+﻿local _,ns = ...
+--hooksecurefunc(DamageMeterEntryMixin, "Init", function(self)
+----EventRegistry:RegisterFrameEventAndCallback("UNIT_SPELLCAST_SUCCEEDED", function(event,unit,guid,spellid)
+ns.event("PLAYER_ENTERING_WORLD", function()
+
+
+end)
