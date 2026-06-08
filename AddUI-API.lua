a7698fe3725012a7459c4508d56f6659fd3e2619
arciay <32655163@qq.com>
2026-06-09 02:40:13 +0800

    更正驱散颜色变量名为 dispelColor

--- AddUI-API.lua
+++ AddUI-API.lua
@@ -180,14 +180,14 @@ function ns.AddText(frame,size)
 end
 
 --驱散颜色
-ns.discolor = C_CurveUtil.CreateColorCurve()
-ns.discolor:SetType(Enum.LuaCurveType.Step)
-ns.discolor:AddPoint(0, CreateColor(0,  0,  0,  0))--无
-ns.discolor:AddPoint(1, CreateColor(1,  1,  1,  1))--魔法
-ns.discolor:AddPoint(2, CreateColor(0.5,0,  1,  1))--诅咒
-ns.discolor:AddPoint(3, CreateColor(1,0.5,  0,  1))--疾病
-ns.discolor:AddPoint(4, CreateColor(0,  1,  0,  1))--中毒
-ns.discolor:AddPoint(9, CreateColor(1,  0,  0,  1))--激怒
+ns.dispelColor = C_CurveUtil.CreateColorCurve()
+ns.dispelColor:SetType(Enum.LuaCurveType.Step)
+ns.dispelColor:AddPoint(0, CreateColor(0,  0,  0,  0))--无
+ns.dispelColor:AddPoint(1, CreateColor(1,  1,  1,  1))--魔法
+ns.dispelColor:AddPoint(2, CreateColor(0.5,0,  1,  1))--诅咒
+ns.dispelColor:AddPoint(3, CreateColor(1,0.5,  0,  1))--疾病
+ns.dispelColor:AddPoint(4, CreateColor(0,  1,  0,  1))--中毒
+ns.dispelColor:AddPoint(9, CreateColor(1,  0,  0,  1))--激怒
 
 --格式化
 ns.buffFmt = C_StringUtil.CreateNumericRuleFormatter()
