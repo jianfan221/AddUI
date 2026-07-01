16cbc1b317289c62a767ddf81c470efa2ac3ad1a
arciay <32655163@qq.com>
2026-07-01 16:08:12 +0800

    新增修复暴雪原生框架文件

---
+++ FixBlizzard.lua
@@ -0,0 +1,19 @@
+﻿local _,ns = ...
+
+--12.07更新后,切换地图偶尔出现以下窗口被隐藏
+local showtable = {
+    "DamageMeter",              --伤害统计
+    "BuffIconCooldownViewer",   --冷却管理器BUFF
+    "EssentialCooldownViewer",  --冷却管理器重要技能
+    "UtilityCooldownViewer",    --冷却管理器次要技能
+    "BuffBarCooldownViewer",    --冷却管理器BUFF条
+}
+
+EventRegistry:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", function()
+    for i, v in ipairs(showtable) do
+        local f = _G[v]
+        if f then
+            f:Show()
+        end
+    end
+end)
