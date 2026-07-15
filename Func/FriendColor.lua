3b0cc45c474100909fcfeb89b4f59b0efd1bd64a
arciay <32655163@qq.com>
2026-07-15 13:01:12 +0800

    新增 SocialUI 好友卡片重排与隐藏战网ID，旧代码后移标注 12.1 后可删除

--- Func/FriendColor.lua
+++ Func/FriendColor.lua
@@ -1,6 +1,104 @@
 ﻿local _,_,_,mygame = GetBuildInfo()
 local _,ns = ...
-ns.tips("好友列表染色显示服务器")
+
+ns.tips("好友列表卡片重新布局")
+if FriendsListSocialCardMixin and FriendsListSocialCardMixin.Initialize then
+    local orig = FriendsListSocialCardMixin.Initialize
+    FriendsListSocialCardMixin.Initialize = function(self, node)
+        orig(self, node)
+        self:SetHeight(35)
+
+        -- 重排文字：一行 FriendName Name，二行 Level Class Location
+        self.FriendName:SetPoint("TOPLEFT", self.TextHolder, "TOPLEFT", 5, 8)
+        self.Name:ClearAllPoints()
+        self.Name:SetPoint("BOTTOMLEFT", self.FriendName, "BOTTOMRIGHT", 4, 0)
+        self.Name:SetPoint("BOTTOM", self.FriendName)
+        self.Name:SetWidth(0)
+        self.Name:SetFontObject(self.FriendName:GetFontObject())
+        self.StateDisplay:ClearAllPoints()
+        self.StateDisplay:SetPoint("BOTTOMLEFT", self.Name, "BOTTOMRIGHT", -2, -1)
+
+        self.Level:Hide()
+        self.Class:Hide()
+
+        self.Location:ClearAllPoints()
+        self.Location:SetPoint("TOPLEFT", self.FriendName, "BOTTOMLEFT", 0, -2)
+
+        -- Level、Class、realm 都用 Location 的颜色（FRIENDS_GRAY_COLOR）
+        local gameAccountInfo = self.elementData and self.elementData.accountInfo and self.elementData.accountInfo.gameAccountInfo
+        if gameAccountInfo then
+            local parts = {}
+            if gameAccountInfo.areaName then tinsert(parts, gameAccountInfo.areaName) end
+            if gameAccountInfo.realmDisplayName then tinsert(parts, gameAccountInfo.realmDisplayName) end
+            if gameAccountInfo.characterLevel then tinsert(parts, gameAccountInfo.characterLevel) end
+            if #parts > 0 then
+                self.Location:SetText(FRIENDS_GRAY_COLOR:WrapTextInColorCode(table.concat(parts, " ")))
+            end
+        end
+
+        -- Name 改为职业颜色
+        if gameAccountInfo and gameAccountInfo.characterName and gameAccountInfo.classFilename and gameAccountInfo.classFilename ~= "" then
+            local color = GetClassColorObj(gameAccountInfo.classFilename)
+            if color then
+                self.Name:SetText(color:WrapTextInColorCode(gameAccountInfo.characterName))
+            end
+        end
+    end
+end
+local view = SocialUIFrame and SocialUIFrame.FriendsList
+if view and view.GetTemplateExtent then
+    local orig = view.GetTemplateExtent
+    view.GetTemplateExtent = function(self, template)
+        local extent = orig(self, template)
+        if template == "FriendsListSocialCardTemplate" then
+            return extent / 2
+        end
+        return extent
+    end
+end
+
+ns.tips("新版好友列表隐藏战网ID")
+if SocialUIFrame then
+    local tagFrame = SocialUIFrame.BattleNetBar and SocialUIFrame.BattleNetBar.ControlsContainer and SocialUIFrame.BattleNetBar.ControlsContainer.PersonalBattleTagDisplay
+    if tagFrame then
+        local hidden = true
+        local cover = CreateFrame("Button", nil, tagFrame)
+        cover:SetAllPoints()
+        cover:RegisterForClicks("AnyUp")
+        cover:SetScript("OnClick", function()
+            hidden = not hidden
+            tagFrame.DisplayText:SetShown(not hidden)
+            tagFrame.CopyBattleTagToClipboardButton:SetShown(not hidden)
+        end)
+        SocialUIFrame:HookScript("OnShow", function()
+            tagFrame.DisplayText:Hide()
+            tagFrame.CopyBattleTagToClipboardButton:Hide()
+        end)
+    end
+end
+
+ns.tips("隐藏好友列表战网ID,点击时显示") -- 12.1 正式上线后可删除（已被 SocialUI 取代）
+--隐藏好友列表战网ID,点击时显示
+local HideBattleIDclick = true
+local HideBattleID = CreateFrame("Button", "HideBattleID", FriendsFrameBattlenetFrame);
+HideBattleID:SetAllPoints(FriendsFrameBattlenetFrame); 
+HideBattleID:RegisterForClicks("AnyUp");
+HideBattleID:SetScript("OnClick", function() 
+	if FriendsFrameBattlenetFrame.Tag:IsShown() then
+		HideBattleIDclick = true
+		FriendsFrameBattlenetFrame.Tag:Hide()
+	else
+		HideBattleIDclick = nil
+		FriendsFrameBattlenetFrame.Tag:Show()
+	end
+end)
+FriendsFrameBattlenetFrame.Tag:HookScript("OnShow", function(self)
+	if HideBattleIDclick then
+		self:Hide()
+	end
+end)
+
+ns.tips("好友列表染色显示服务器") -- 12.1 正式上线后可删除（已被 SocialUI 取代）
 ns.hook("FriendsFrame_UpdateFriendButton", function(friendbutton)
 	if not FriendsListFrame or not FriendsListFrame:IsShown() then return end
 	if not friendbutton.id then return end
@@ -74,23 +172,3 @@ end)
 
 
 
-ns.tips("隐藏好友列表战网ID,点击时显示")
---隐藏好友列表战网ID,点击时显示
-local HideBattleIDclick = true
-local HideBattleID = CreateFrame("Button", "HideBattleID", FriendsFrameBattlenetFrame);
-HideBattleID:SetAllPoints(FriendsFrameBattlenetFrame); 
-HideBattleID:RegisterForClicks("AnyUp");
-HideBattleID:SetScript("OnClick", function() 
-	if FriendsFrameBattlenetFrame.Tag:IsShown() then
-		HideBattleIDclick = true
-		FriendsFrameBattlenetFrame.Tag:Hide()
-	else
-		HideBattleIDclick = nil
-		FriendsFrameBattlenetFrame.Tag:Show()
-	end
-end)
-FriendsFrameBattlenetFrame.Tag:HookScript("OnShow", function(self)
-	if HideBattleIDclick then
-		self:Hide()
-	end
-end)
