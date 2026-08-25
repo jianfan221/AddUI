108e6ff94d7bb6c2781f5b3f4721a9da8e4abd2d
arciay <32655163@qq.com>
2026-08-25 13:40:20 +0800

    启用地下城手册套装分页并美化代码

--- FixBlizzard.lua
+++ FixBlizzard.lua
@@ -1,39 +1,46 @@
-﻿local _,ns = ...
+﻿local _, ns = ...
 
---12.07更新后,切换地图偶尔出现以下窗口被隐藏
-local showtable = {
-    "DamageMeter",              --伤害统计
-    "BuffIconCooldownViewer",   --冷却管理器BUFF
-    "EssentialCooldownViewer",  --冷却管理器重要技能
-    "UtilityCooldownViewer",    --冷却管理器次要技能
-    "BuffBarCooldownViewer",    --冷却管理器BUFF条
+-- 12.07 更新后，切换地图偶尔出现以下窗口被隐藏
+local showTable = {
+    "DamageMeter",              -- 伤害统计
+    "BuffIconCooldownViewer",   -- 冷却管理器 BUFF
+    "EssentialCooldownViewer",  -- 冷却管理器重要技能
+    "UtilityCooldownViewer",    -- 冷却管理器次要技能
+    "BuffBarCooldownViewer",    -- 冷却管理器 BUFF 条
 }
+
 EventRegistry:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", function()
-    for i, v in ipairs(showtable) do
-        local f = _G[v]
-        if f then
-            local isDM = v == "DamageMeter"
-            local vis = isDM and f.visibility or f.visibleSetting
-            if vis == 0 and C_CVar.GetCVar(isDM and "damageMeterEnabled" or "cooldownViewerEnabled") == "1" then
-                f:Show()
+    for _, name in ipairs(showTable) do
+        local frame = _G[name]
+        if frame then
+            local isDamageMeter = name == "DamageMeter"
+            local visible = isDamageMeter and frame.visibility or frame.visibleSetting
+            local cvar = isDamageMeter and "damageMeterEnabled" or "cooldownViewerEnabled"
+
+            if visible == 0 and C_CVar.GetCVar(cvar) == "1" then
+                frame:Show()
             end
         end
     end
 end)
 
-
---12.1更新后各种帮助气泡 拦截掉参考https://addons.wago.io/addons/hide-help-frames
+-- 12.1 更新后，拦截各种帮助气泡
+-- 参考：https://addons.wago.io/addons/hide-help-frames
 if HelpTipTemplateMixin and HelpTipTemplateMixin.OnShow then
     hooksecurefunc(HelpTipTemplateMixin, "OnShow", function(self)
         self:Hide()
     end)
 end
 
+-- 启用地下城手册的套装分页
+EventUtil.ContinueOnAddOnLoaded("Blizzard_EncounterJournal", function()
+    EncounterJournal:HookScript("OnShow", function(self)
+        PanelTemplates_SetAllTabsShown(self, true)
+        PanelTemplates_SetNumTabs(self, #self.Tabs)
+    end)
+end)
 
 -- 屏蔽空的 CHAT_MSG_SYSTEM 消息（空行会导致聊天框信息被莫名压缩）
 ChatFrameUtil.AddMessageEventFilter("CHAT_MSG_SYSTEM", function(self, event, msg, ...)
-    if msg == "" then
-        return true
-    end
-    return false
+    return msg == ""
 end)
